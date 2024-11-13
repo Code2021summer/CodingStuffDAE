ba6b93c10d59b45ec0ac80413e1844bc9bf66d05
Code2021summer <moonadi0@gmail.com>
2024-11-13 18:00:52 -0500

    Started python project

--- python_1/first.py
+++ python_1/first.py
@@ -1,3 +1,34 @@
-print ("Let's applesauce")
-sauceCount = input ("How many applesauces do you need? ")
-print("You wanted", sauceCount)
+def addNum(firstNum, secondNum):
+    sum = firstNum + secondNum
+    print("The sum of your numbers is" , sum)
+def subtractNum(firstNum, secondNum):
+    difference = firstNum - secondNum
+    print("The difference of your numbers is" , difference)
+def multiplyNum(firstNum, secondNum):
+    product = firstNum * secondNum
+    print("The product of your numbers is" , product)
+def divideNum(firstNum, secondNum):
+    quotient = firstNum / secondNum
+    print("The quotient of your numbers is" , quotient)
+
+def displayMenu():
+    print("1. Additon")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+def main(): #main program
+    print("I shall give you answers for math")
+    displayMenu()
+    userChoice = int(input("Pick one. Just type the number (or else...): "))
+    userFirstNum = int(input("Give me a number: "))
+    userSecondNum = int(input("Give me another number: "))
+    if userChoice == 1:
+        addNum(userFirstNum, userSecondNum)
+    if userChoice == 2:
+        subtractNum(userFirstNum, userSecondNum)
+    if userChoice == 3:
+        multiplyNum(userFirstNum, userSecondNum)
+    if userChoice == 4:
+        divideNum(userFirstNum, userSecondNum)
+main()
---
+++ python_1/project.py
@@ -0,0 +1,14 @@
+def presentSituation(scenario, optionOne, optionTwo, optionThree):
+    print(scenario)
+    print("1.", optionOne)
+    print("2." , optionTwo)
+    print("3." , optionThree)
+    input("What do you do? ")
+def chooseSituation(situationNumber):
+    if situationNumber == 1:
+        
+def main():
+    print("Type the number to choose your response")
+    chooseSituation()
+    presentSituation()
+main()
