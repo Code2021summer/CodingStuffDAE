33ee049eb74af3d79ec5efc7358892ddda4b9ef5
Code2021summer <moonadi0@gmail.com>
2025-03-03 18:10:16 -0500

    Made a clicker game that rickrolls you

--- javascript_1/index.js
+++ javascript_1/index.js
@@ -1,2 +1,21 @@
-document.getElementById("clicker").innerText = "HEIHFIO";
+document.getElementById("clicker").innerText = "CLICK ME";
 console.log("JavaScript is working!");
+let clicks = 0;
+const clicker = document.getElementById('clicker');
+clicker.addEventListener('click', function() {
+    //window.alert("the clicker clicks");
+    clicks = clicks + 1;
+    //window.alert(clicks);
+    if (clicks==100) {
+        document.getElementById("clicker").innerHTML = "YO YOU DID IT"; 
+        clicker.onclick = function(){
+            window.location.href = "https://shattereddisk.github.io/rickroll/rickroll.mp4";
+        };
+    } 
+    if (clicks == 1) {
+        document.getElementById("clickCount").innerText = clicks + " click";
+    }
+    else
+        document.getElementById("clickCount").innerText = clicks + " clicks";
+});
+
