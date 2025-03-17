efbbf77ffbeb9fe4bd00fb69842db89b15575118
Code2021summer <moonadi0@gmail.com>
2025-03-17 16:58:13 -0400

    Started javascript

--- javascript_1/index.js
+++ javascript_1/index.js
@@ -7,7 +7,7 @@ clicker.addEventListener('click', function() {
     clicks = clicks + 1;
     //window.alert(clicks);
     if (clicks==5) {
-        document.getElementById("clicker").innerHTML = "YO YOU DID IT"; 
+        document.getElementById("clickCount").innerText = "YO YOU DID IT"; 
         clicker.onclick = function(){
             window.location.href = "https://shattereddisk.github.io/rickroll/rickroll.mp4";
         };
