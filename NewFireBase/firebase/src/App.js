41c31dfd9aa395d04b5221fd8c3d5d8e5d3047aa
unknown <moonadi0@gmail.com>
2025-05-16 12:58:55 -0400

    added fireStore to project

--- NewFireBase/firebase/src/App.js
+++ NewFireBase/firebase/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Auth from "./components/auth"
+import Post from "./components/post";
 function App() {
   return (
     <div className="App">
       <Auth></Auth>
+      <Post></Post>
     </div>
   );
 }
